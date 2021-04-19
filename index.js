bc020eb31a0d1bc1f3f80cefae51c6694d7f7583
Dan Price <danielparkerprice@gmail.com>
2021-04-19 11:08:29 +0100

    setup

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 4000
+const dbURI = 'mongodb://localhost/terroirist-db'
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    console.log('Mongo has connected')
+  })
 
 
 app.listen(port, () => console.log(`Listening on Port: ${port}`))
