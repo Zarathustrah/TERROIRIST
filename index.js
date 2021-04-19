50bcac82d6d5fdb65b3d62e48cacdd39271a8f70
Dan Price <danielparkerprice@gmail.com>
2021-04-19 19:00:33 +0100

    init mongoDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+// const { truncate } = require('node:fs')
 const app = express()
 const port = 4000
 const dbURI = 'mongodb://localhost/terroirist-db'
@@ -11,6 +12,19 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true, useUnifie
       return
     }
     console.log('Mongo has connected')
+  })
+
+  const wineSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    price: { type: Number, required: true }, 
+    size: { type: String, required: true },
+    vintage: { type: String, required: true },
+    type: { type: String, required: true },
+    country: { type: String, required: true },
+    region: { type: String, required: true },
+    abv: { type: String, required: true },
+    description: { type: String, required: true }
+
   })
 
 
