0987c57ad6bc46ad888c5fb8302edb962d14f46b
Dan Price <danielparkerprice@gmail.com>
2021-04-26 17:18:04 +0100

    token auth

--- config/router.js
+++ config/router.js
@@ -2,15 +2,16 @@
 const router = require('express').Router()
 const wines = require('../controllers/wines')
 const auth = require('../controllers/auth')
+const secureRoute = require('../lib/secureRoute')
 
 router.route('/wines')
   .get(wines.index)
-  .post(wines.create)
+  .post(secureRoute, wines.create)
 
 router.route('/wines/:id')
   .get(wines.show)
-  .put(wines.edit)
-  .delete(wines.delete)
+  .put(secureRoute, wines.edit)
+  .delete(secureRoute, wines.delete)
 
 router.route('/register')
   .post(auth.register)
