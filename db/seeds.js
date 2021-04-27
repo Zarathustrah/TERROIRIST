7015967988e342715a50847b72f2bb5bac52df23
Dan Price <danielparkerprice@gmail.com>
2021-04-27 19:32:12 +0100

    seeds debug

--- db/seeds.js
+++ db/seeds.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose')
 const { dbURI } = require('../config/environment')
 
 const Wine = require('../models/wine')
-const User = require('../models/user')
+// const User = require('../models/user')
 
 const wineData = require('./data/wines')
-const usersData = require('./data/users')
+// const usersData = require('./data/users')
 
 
 mongoose.connect(dbURI, 
@@ -22,8 +22,8 @@ mongoose.connect(dbURI,
       
       console.log('Database Dropped')
 
-      const users = await User.create(usersData)
-      console.log(`${users.length} Users Created`)
+      // const users = await User.create(usersData)
+      // console.log(`${users.length} Users Created`)
 
       const winesWithUsers = wineData.map(wine => {
         wine.user = users[0]._id
