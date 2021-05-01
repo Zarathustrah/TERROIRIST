a365c7115daef15c193a015f755d888aff593e1f
Dan Price <danielparkerprice@gmail.com>
2021-05-01 22:49:51 +0100

    fe file setup

---
+++ frontend/src/components/common/ErrorPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ErrorPage = () => (
+  <h1>Error Page</h1>
+)
+
+export default ErrorPage
---
+++ frontend/src/components/wines/WineIndex.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const WineIndex = () => (
+  <h1>Wine index</h1>
+)
+
+export default WineIndex
