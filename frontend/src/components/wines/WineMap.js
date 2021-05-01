3192900e4382bf87e1ea42a5a04b3a5ba1050301
Dan Price <danielparkerprice@gmail.com>
2021-05-01 23:25:58 +0100

    fe file setup

---
+++ frontend/src/components/wines/WineMap.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const WineMap = () => (
+  <h1>WineMap </h1>
+)
+
+export default WineMap 
---
+++ frontend/src/components/wines/WineShow.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const WineShow = () => (
+  <h1>Wine Show</h1>
+)
+
+export default WineShow 
