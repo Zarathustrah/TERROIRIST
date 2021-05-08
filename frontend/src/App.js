e0810db22212247bd202bd37421e26fb21dc2753
Dan Price <danielparkerprice@gmail.com>
2021-05-08 19:12:37 +0100

    file setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,12 @@
-function App() {
-  return <h1>Hello World</h1>
+import React from 'react'
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1>Hello World</h1>
+    )
+  }
+
 }
 
 export default App
