e2c0b52f621e91a76d794dd992564376c4a46ced
Dan Price <danielparkerprice@gmail.com>
2021-05-19 14:18:19 +0100

    seed db extended

--- db/data/wines.js
+++ db/data/wines.js
@@ -42,5 +42,42 @@ module.exports = [
     "image": "https://www.dbmwines.co.uk/pics/big_431.jpg",
     "description": "A rich and concentrated Valpolicella with aromas of peppery tobacco and spice. Rich red fruits on the round and balanced palate combine with savoury notes through to a long, luscious finish. We started working with Roccolo Grassi 10 years ago and have been consistently impressed with their outstanding quality. Marco Sartori is a driven man and in a few short years he has gone from making his first vintage to becoming one of the top talents in the region.",
     "retailer": "https://www.wineman.co.uk/product/roccolo-grassi-valpolicella-superiore"
+  },
+  { 
+    "producer": "Peter Sissek",
+    "name": "Psi",
+    "vintage": 2017,
+    "type": "red",
+    "country": "Spain",
+    "region": "Ribera del Duero",
+    "image": "https://hedonism.co.uk/sites/default/files/styles/product_large/public/vintner/vintner-images/HED57401.JPG?itok=nfs6nGtu",
+    "description": "Peter Sisseck is famous for making Pingus, an iconic wine that many consider to be the greatest Spanish wine of all. Psi uses biodynamic bush-vine Tempranillo grapes from the hot, dry Ribera del Duero region. Fantastic, deep and intense with ripe, plum, spice flavours – perfect with Beef!",
+    "retailer": "https://hedonism.co.uk/product/psi-peter-sisseck-2017"
+  },
+  { 
+    "producer": "Bodegas Montecillo",
+    "name": "Viña Monty Graciano",
+    "vintage": 2015,
+    "type": "red",
+    "country": "Spain",
+    "region": "Rioja",
+    "image": "https://hedonism.co.uk/sites/default/files/styles/product_large/public/vintner/vintner-images/HED31851.JPG?itok=_J927oii",
+    "description": "Peter Sisseck is famous for making Pingus, an iconic wine that many consider to be the greatest Spanish wine of all. Psi uses biodynamic bush-vine Tempranillo grapes from the hot, dry Ribera del Duero region. Fantastic, deep and intense with ripe, plum, spice flavours – perfect with Beef!",
+    "retailer": "https://hedonism.co.uk/product/vina-monty-graciano-2015"
+  },
+  { 
+    "producer": "Dirty & Rowdy",
+    "name": "Familiar Mourvèdre",
+    "vintage": 2015,
+    "type": "red",
+    "country": "United States",
+    "region": "California",
+    "image": "https://hedonism.co.uk/sites/default/files/styles/product_large/public/vintner/vintner-images/HED68326.JPG?itok=JO5kFt_c",
+    "description": "Tried, true, and your go to. Dirty & Rowdy's most popular wine from the wild side of the woods. It is energetic, thirst quenching and shows the full spectrum of Mourvèdre from blood orange to roasted game. Too much around the house is never quite enough. This is your portal to the astral plane - break on through!",
+    "retailer": "https://hedonism.co.uk/product/dirty-rowdy-familiar-mourvedre-2015"
   }
+
+  
+
+
 ]
