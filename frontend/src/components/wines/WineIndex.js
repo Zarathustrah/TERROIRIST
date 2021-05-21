94262d3674f179f516bcb2d6750e311ea3eb41e7
Dan Price <danielparkerprice@gmail.com>
2021-05-21 13:45:29 +0100

    fe setup

--- frontend/src/components/wines/WineIndex.js
+++ frontend/src/components/wines/WineIndex.js
@@ -1,31 +1,52 @@
 import React from 'react'
 import axios from 'axios'
 
-class WineIndex extends React.Component {
-
-  state = {
-    wines: [],
-  }
 
 
-  async componentDidMount() {
-    try {
+function WindeIndex() {
+
+  React.useEffect(() => {
+    console.log('Rendered')
+    const getData = async () => {
       const res = await axios.get('/api/wines')
-      this.setState({ wines: res.data })
-    } catch (err) {
-      console.log(err)
+      console.log(res.data)
     }
-  }
+    getData()  
+  }, [])
 
-  render() {
-    console.log(this.state.wines)
-    return (
-      <h1>Wine Index Page</h1>
-    )
-  }
+  return (
+    <h1>WineIndex</h1>
+  )
+
 }
 
-export default WineIndex
+export default WindeIndex
+
+// class WineIndex extends React.Component {
+
+//   state = {
+//     wines: [],
+//   }
+
+
+//   async componentDidMount() {
+//     try {
+//       const res = await axios.get('/api/wines')
+//       this.setState({ wines: res.data })
+//     } catch (err) {
+//       console.log(err)
+//     }
+//   }
+
+//   render() {
+//     console.log(this.state.wines)
+//     return (
+//       <h1>Wine Index Page</h1>
+//     )
+//   }
+// }
+
+// export default WineIndex
 
 
 
