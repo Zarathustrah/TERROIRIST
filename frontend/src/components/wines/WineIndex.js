7ccfe69646cf2e4b9d8614cbc37cc5aafc37a590
Dan Price <danielparkerprice@gmail.com>
2021-05-21 21:25:09 +0100

    fe hooks setup

--- frontend/src/components/wines/WineIndex.js
+++ frontend/src/components/wines/WineIndex.js
@@ -1,26 +1,32 @@
 import React from 'react'
 import axios from 'axios'
 
+function WinesIndex() {
+  const [wines, setWines] = React.useState([])
 
-
-function WindeIndex() {
 
   React.useEffect(() => {
-    console.log('Rendered')
     const getData = async () => {
-      const res = await axios.get('/api/wines')
-      console.log(res.data)
+      try {
+        const { data } = await axios.get('./api/wines')
+        setWines(data)
+      } catch (err) {
+        console.log(err)
+      }
     }
-    getData()  
+    getData()
   }, [])
+  
 
+
+  console.log(wines)
   return (
-    <h1>WineIndex</h1>
+    <h1>WineIndex: </h1>
   )
 
 }
 
-export default WindeIndex
+export default WinesIndex
 
 // class WineIndex extends React.Component {
 
