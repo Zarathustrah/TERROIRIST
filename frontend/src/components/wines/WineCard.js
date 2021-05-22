49432e54c7970e2de738e9c2ca1cfe9366ec312a
Dan Price <danielparkerprice@gmail.com>
2021-05-22 20:45:51 +0100

    wine card edit

--- frontend/src/components/wines/WineCard.js
+++ frontend/src/components/wines/WineCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const WineCard = ({ name, producer, vintage, image }) => (
   <div className="grid grid-cols-4">
-    <div className="bg-blue-100 gap-y-12">
+    <div className="bg-coolGrey-100 gap-y-12">
       <img src={image} alt={name} width="100" height="130"/>
       <h3>{producer}<br/>{name}</h3>
       <p>{vintage}</p>
