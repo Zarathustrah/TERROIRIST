ff559d82a014a60de1e2cbd8a02e19264178391a
Dan Price <danielparkerprice@gmail.com>
2021-05-22 20:55:11 +0100

    home page hero

--- frontend/src/components/common/Home.js
+++ frontend/src/components/common/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Home = () => (
   <div className="min-h-screen bg-blue-50">
+    <img src="https://i.ibb.co/8smsy1w/maja-petric-v-GQ49l9-I4-EE-unsplash.jpg" alt=""/>
 
   </div>
 )
