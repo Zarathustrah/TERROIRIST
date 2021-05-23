c86de3e18807fe5001ec9c57155cc21a30f8d774
Dan Price <danielparkerprice@gmail.com>
2021-05-23 16:55:55 +0100

    emmet added

--- frontend/src/components/wines/WineCard.js
+++ frontend/src/components/wines/WineCard.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 const WineCard = ({ name, producer, vintage, image }) => (
   <div className="grid grid-cols-4">
-    <div className="bg-coolGrey-100 gap-y-12">
+    <div className="bg-coolGre y-100 gap-y-12">
       <img src={image} alt={name} width="100" height="130"/>
       <h3>{producer}<br/>{name}</h3>
-      <p>{vintage}</p>      
+      <p>{vintage}</p> 
+      <h1 className="text-6">Hello</h1>  
     </div>   
-  </div>
-)
+  </div> 
+)  
 
 export default WineCard
