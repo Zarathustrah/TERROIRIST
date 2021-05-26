aadbb9f123c1437d0727a0b9a882d193e32107d7
Dan Price <danielparkerprice@gmail.com>
2021-05-26 10:13:59 +0100

    search filter debugged

--- frontend/src/components/common/WineSearch.js
+++ frontend/src/components/common/WineSearch.js
@@ -18,7 +18,7 @@ const WineSearch = ({ searchText }) => {
       <form className="w-full max-w-sm">
         <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
           <input onChange={handleChange} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search" />
-          <button onSubmit={handleSubmit} className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-grey py-1 px-2 rounded" type="submit">Submit</button>
+          <button onClick={handleSubmit} className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-grey py-1 px-2 rounded" type="submit">Submit</button>
         </div>
       </form>
     </div>
