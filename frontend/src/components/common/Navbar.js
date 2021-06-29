46f242b8dd505d0ea02dd76755720a0cd71e67af
Dan Price <danielparkerprice@gmail.com>
2021-06-29 11:26:55 +0100

    navbar

--- frontend/src/components/common/Navbar.js
+++ frontend/src/components/common/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
     <div>
       <nav className="bg-gray-800">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
+          <div className="flex flex-row-reverse items-center justify-between h-16">
             <div className="flex items-center">
               <div className="flex-shrink-0">
                 {/* <img
