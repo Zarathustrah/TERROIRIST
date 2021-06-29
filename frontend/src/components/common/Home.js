36e79ae4d6096d1463725f771ead553f832d0595
Dan Price <danielparkerprice@gmail.com>
2021-06-29 15:22:20 +0100

    home page

--- frontend/src/components/common/Home.js
+++ frontend/src/components/common/Home.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Hero from '../../assets/Hero.jpg'
 
 function Home() {
   return (
     <div className="min-h-full bg-gray-100 font-body">
       <div className="h-screen flex flex-col">
         <Navbar />
-
+        <div
+          className="
+            relative flex items-center justify-center h-full
+            bg-no-repeat bg-cover bg-center bg-fixed          
+          "
+          style={{
+            backgroundImage: `url(${Hero})`,
+          }}
+        >
+          <h1 className="text-white uppercase text-xl md:text-9xl">terroirista</h1>
+        </div>
       </div>
     </div>
   )
