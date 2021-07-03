4d40a1dcff5b305b82e7f0f4e7b903198db5c2d0
Dan Price <danielparkerprice@gmail.com>
2021-07-03 15:09:45 +0100

    uder model deleted

--- db/data/users.js
+++ db/data/users.js
@@ -1,32 +1,32 @@
-module.exports = [
-  {
-    username: "Dan",
-    email: "dan@mail.com",
-    profileImage: "https://hedonism.co.uk/sites/default/files/styles/product_large/public/vintner/vintner-images/HED55622.JPG?itok=mtI2Yk_6",
-    password: "pass", 
-    passwordConfirmation: "pass"
-  },
-  {
-    username: "Tom",
-    email: "tom@mail.com",
-    profileImage: "https://hedonism.co.uk/sites/default/files/styles/product_large/public/vintner/vintner-images/HED55622.JPG?itok=mtI2Yk_6",
-    password: "pass", 
-    passwordConfirmation: "pass"
-  },
-  {
-    username: "Claire",
-    email: "claire@mail.com",
-    profileImage: "https://hedonism.co.uk/sites/default/files/styles/product_large/public/vintner/vintner-images/HED55622.JPG?itok=mtI2Yk_6",
-    password: "pass", 
-    passwordConfirmation: "pass"
-  },
-  {
-    username: "Joss",
-    email: "joss@mail.com",
-    profileImage: "https://hedonism.co.uk/sites/default/files/styles/product_large/public/vintner/vintner-images/HED55622.JPG?itok=mtI2Yk_6",
-    password: "pass", 
-    passwordConfirmation: "pass"
-  }
+// module.exports = [
+//   {
+//     username: "Dan",
+//     email: "dan@mail.com",
+//     profileImage: "https://hedonism.co.uk/sites/default/files/styles/product_large/public/vintner/vintner-images/HED55622.JPG?itok=mtI2Yk_6",
+//     password: "pass", 
+//     passwordConfirmation: "pass"
+//   },
+//   {
+//     username: "Tom",
+//     email: "tom@mail.com",
+//     profileImage: "https://hedonism.co.uk/sites/default/files/styles/product_large/public/vintner/vintner-images/HED55622.JPG?itok=mtI2Yk_6",
+//     password: "pass", 
+//     passwordConfirmation: "pass"
+//   },
+//   {
+//     username: "Claire",
+//     email: "claire@mail.com",
+//     profileImage: "https://hedonism.co.uk/sites/default/files/styles/product_large/public/vintner/vintner-images/HED55622.JPG?itok=mtI2Yk_6",
+//     password: "pass", 
+//     passwordConfirmation: "pass"
+//   },
+//   {
+//     username: "Joss",
+//     email: "joss@mail.com",
+//     profileImage: "https://hedonism.co.uk/sites/default/files/styles/product_large/public/vintner/vintner-images/HED55622.JPG?itok=mtI2Yk_6",
+//     password: "pass", 
+//     passwordConfirmation: "pass"
+//   }
   
 
-]
+// ]
